0599e19e2bd486c14d8baaa77f47c496769c7161
mann0777 <mannamloh77@gmail.com>
2022-09-10 12:26:03 +0530

    Created Sdata using array

---
+++ src/Sdata.jsx
@@ -0,0 +1,28 @@
+const Sdata=[
+    {
+        imgsrc: web,
+        title: "web develpment",
+    },
+    {
+        imngsrc: app,
+        title: "App development",
+    },
+    {
+        imgsrc: software,
+        title: "sorftware",
+    },
+    {
+        imgsrc: software,
+        title: "sorftware",
+    },
+    {
+        imgsrc: software,
+        title: "sorftware",
+    },
+    {
+        imgsrc: software,
+        title: "sorftware",
+    },
+]
+
+export default Sdata;
--- src/Service.jsx
+++ src/Service.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import Sdata from "./Sdata";
+import Card from "./Card";
 
 const Service = ()=>{
     return(
         <>
-            <h1>Service</h1>
+            <div className="my-5">
+                <h1 className="text-center" >Our Services</h1>
+            </div>
+            <div className="container-fluid mb-5">
+                <div className="row">
+                    <div className="col-10 mx-auto">
+                    <div className="row gy-4">
+                  <Card/>
+                    </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 
