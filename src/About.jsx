01c03491740aed050de7e458b52bcfe64f3e02e1
mann0777 <mannamloh77@gmail.com>
2022-09-14 11:20:30 +0530

    Used arrow map function and jason file

--- src/About.jsx
+++ src/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import web from "../src/images/Cars.png";
+import web from "../src/images/luxury.jpg";
 import Common from "./Common";
 
 const About = ()=>{
--- src/Sdata.jsx
+++ src/Sdata.jsx
@@ -1,27 +1,33 @@
+import Accord from "../src/images/Accord.jpg";
+import Bmw from "../src/images/Bmw.jpg";
+import Mazda from "../src/images/Mazda.jpg";
+import porsche from "../src/images/porsche.jpg";
+import Rav4 from "../src/images/Rav4.jpg";
+import Volkswagen from "../src/images/Volkswagen.jpg";
 const Sdata=[
     {
-        imgsrc: web,
-        title: "web develpment",
+        imgsrc: Accord,
+        title: "Accord Development",
     },
     {
-        imngsrc: app,
-        title: "App development",
+        imgsrc: Bmw,
+        title: "Bmw Development",
     },
     {
-        imgsrc: software,
-        title: "sorftware",
+        imgsrc: porsche,
+        title: "Porsche Devlopment",
     },
     {
-        imgsrc: software,
-        title: "sorftware",
+        imgsrc: Rav4,
+        title: "Rav4 Development",
     },
     {
-        imgsrc: software,
-        title: "sorftware",
+        imgsrc: Volkswagen,
+        title: "Volksagen Development",
     },
     {
-        imgsrc: software,
-        title: "sorftware",
+        imgsrc: Mazda,
+        title: "Mazda Development",
     },
 ]
 
